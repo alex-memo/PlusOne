e513aa55be6db98494eaed6cbb2e020af5a63e46
memo <alexis45645@gmail.com>
2022-11-05 16:25:34 -0400

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,7 @@
 
 public class Solution {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//q66
 		// TODO Auto-generated method stub
 int[]a=(plusOne(new int[] {8,9,9,9}));
 int i=0;
